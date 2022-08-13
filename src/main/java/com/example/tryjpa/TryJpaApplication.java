a4bf5ed71398706e0a954b37e25fff5a7714a56c
minami.yoshihiko <nannany@hey.jp>
2022-08-13 14:47:15 +0900

    initial commit

---
+++ src/main/java/com/example/tryjpa/TryJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tryjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tryjpa/TryJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tryjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
